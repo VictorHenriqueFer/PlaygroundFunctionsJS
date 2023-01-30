5e8e33c2525013b23b0d21ace578921d0994f6aa
Victor Henrique Fernandes <victorhenriquefernandes2@gmail.com>
2023-01-30 20:21:24 -0300

    ficando quase doido

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,26 @@
 // Desafio 1 - Crie a função compareTrue
-
+const girafa = true;
+const elefante = true;
+const macaco = false;
+const compareTrue = (parametro1, parametro2) => {
+  if(parametro1 === girafa && parametro2 === elefante) {
+    return true;
+  } else {
+    return false;
+  }
+}
 // Desafio 2 - Crie a função splitSentence
+const splitSentence = (parametro1) => parametro1.split(' ');
 
 // Desafio 3 - Crie a função concatName
 
+const concatName = (par1) => `${par1[par1.length - 1]}, ${par1[0]}`; 
+
 // Desafio 4 - Crie a função footballPoints
+const footballPoints = (vic, emp) => vic * 3 + emp;
 
 // Desafio 5 - Crie a função highestCount
+
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
 
@@ -20,17 +34,17 @@
 
 // Não modifique essas linhas
 module.exports = {
-  calcTriangleArea: typeof calcTriangleArea === 'function' ? calcTriangleArea : (() => {}),
-  calcRectangleArea: typeof calcRectangleArea === 'function' ? calcRectangleArea : (() => {}),
-  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : (() => {}),
-  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : (() => {}),
-  compareTrue: typeof compareTrue === 'function' ? compareTrue : (() => {}),
-  concatName: typeof concatName === 'function' ? concatName : (() => {}),
-  decode: typeof decode === 'function' ? decode : (() => {}),
-  encode: typeof encode === 'function' ? encode : (() => {}),
-  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : (() => {}),
-  footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => {}),
-  highestCount: typeof highestCount === 'function' ? highestCount : (() => {}),
-  splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => {}),
-  techList: typeof techList === 'function' ? techList : (() => {}),
+  calcTriangleArea: typeof calcTriangleArea === 'function' ? calcTriangleArea : (() => { }),
+  calcRectangleArea: typeof calcRectangleArea === 'function' ? calcRectangleArea : (() => { }),
+  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : (() => { }),
+  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : (() => { }),
+  compareTrue: typeof compareTrue === 'function' ? compareTrue : (() => { }),
+  concatName: typeof concatName === 'function' ? concatName : (() => { }),
+  decode: typeof decode === 'function' ? decode : (() => { }),
+  encode: typeof encode === 'function' ? encode : (() => { }),
+  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : (() => { }),
+  footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => { }),
+  highestCount: typeof highestCount === 'function' ? highestCount : (() => { }),
+  splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => { }),
+  techList: typeof techList === 'function' ? techList : (() => { }),
 };
